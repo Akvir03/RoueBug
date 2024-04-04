27f5d409625111295fff1d8e400ded46d05ca942
marinaflament <118009945+Marynah12@users.noreply.github.com>
2024-04-04 16:32:09 +0200

    feat(delete):remove console.log

--- roue-des-choix/MesRoues/script.js
+++ roue-des-choix/MesRoues/script.js
@@ -1,38 +1,8 @@
-// axios
-//   .get("http://localhost:3001/userwheels")
-//   .then((response) => {
-//     const wheelsData = response.data;
-//     console.log(wheelsData);
-//     const tableauMesRoues = document
-//       .getElementById("tableauMesRoues")
-//       .getElementsByTagName("tbody")[0];
-
-//     tableauMesRoues.innerHTML = "";
-
-//     wheelsData.forEach((wheel) => {
-//       const newRow = tableauMesRoues.insertRow();
-
-//       wheel.restaurants.forEach((restaurant) => {
-//         newRow.innerHTML += `<td>${restaurant}</td>`;
-//       });
-//       newRow.innerHTML += `<td>
-//           <button data-id="${wheel._id}" class="btn" onclick=retrieveId()>DELETE</button>
-//         </td>`;
-//     });
-//   })
-//   .catch((error) => {
-//     console.error(
-//       "Une erreur est survenue lors de la récupération des données :",
-//       error
-//     );
-//   });
-
 function fetchAndDisplayWheelsData() {
   axios
     .get("http://localhost:3001/userwheels")
     .then((response) => {
       const wheelsData = response.data;
-      console.log(wheelsData);
       const tableauMesRoues = document
         .getElementById("tableauMesRoues")
         .getElementsByTagName("tbody")[0];
@@ -48,7 +18,6 @@ function fetchAndDisplayWheelsData() {
         newRow.innerHTML += `<td>
           <button data-id="${wheel._id}" class="btn" onclick=retrieveId()>DELETE</button>
         </td>`;
-        console.log(wheel._id);
       });
     })
     .catch((error) => {
@@ -63,7 +32,6 @@ fetchAndDisplayWheelsData();
 
 function deleteWheel(id) {
   const idWheel = { id: id };
-  console.log(idWheel);
   axios
     .delete("http://localhost:3001/userwheels", { params: idWheel })
     .then((response) => {
@@ -77,8 +45,6 @@ function retrieveId() {
   buttons.forEach(function (button) {
     button.addEventListener("click", function () {
       var id = button.getAttribute("data-id");
-      console.log("ID du bouton cliqué : ", id);
-      console.log(id);
       button.removeEventListener("click", arguments.callee);
       deleteWheel(id);
     });
